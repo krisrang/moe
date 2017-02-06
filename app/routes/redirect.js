0ce321f33d7b38614b5bb72a5acab7af6afc30fd
Kristjan Rang <mail@rang.ee>
2017-02-06 18:09:07 +0200

    Fix redirect

--- app/routes/redirect.js
+++ app/routes/redirect.js
@@ -4,10 +4,10 @@ export default Ember.Route.extend({
   redirect(params) {
     let path = params && params.path;
     if (path && path.indexOf('stuff') === 0) {
-      let subpath = path.replace("stuff/", "");
-      window.location.replace("https://files.kris.moe/" + subpath);
+      let subpath = path.replace("stuff", "");
+      window.location.replace("https://files.kris.moe" + subpath);
     } else if (path && path.indexOf('files') === 0) {
-      let subpath = path.replace("files/", "");
+      let subpath = path.replace("files", "");
       window.location.replace("https://files.kris.moe/files" + subpath);
     }
   }
