068b172580465ae5de4c54c6255c9ef3a3a35615
Kristjan Rang <mail@rang.ee>
2017-02-06 18:05:38 +0200

    Add redirect for old paths

---
+++ app/routes/redirect.js
@@ -0,0 +1,14 @@
+import Ember from 'ember';
+
+export default Ember.Route.extend({
+  redirect(params) {
+    let path = params && params.path;
+    if (path && path.indexOf('stuff') === 0) {
+      let subpath = path.replace("stuff/", "");
+      window.location.replace("https://files.kris.moe/" + subpath);
+    } else if (path && path.indexOf('files') === 0) {
+      let subpath = path.replace("files/", "");
+      window.location.replace("https://files.kris.moe/files" + subpath);
+    }
+  }
+});
